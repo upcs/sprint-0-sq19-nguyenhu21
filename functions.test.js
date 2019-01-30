20942c01a9d77f8ea16c4529d26a2c2881d95ce6
nguyenhu21 <nguyenhu21@up.edu>
2019-01-30 00:28:59 -0800

    Added unit test

---
+++ functions.test.js
@@ -0,0 +1,9 @@
+const add = require('./js/functions.js');
+
+test('add 3 + 3 to equal 6', () => {
+	expect(add(3, 3)).toBe(6);
+});
+
+test('add 4 + 6 to equal 10', () => {
+	expect(add(4, 6)).toBe(10);
+});
--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
 function add(p1, p2) {
 	return p1 + p2;
 }
+module.exports = add;
